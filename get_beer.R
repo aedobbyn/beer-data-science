d741de0d97402c80d38915c8a73960af0f49a353
aedobbyn <adobbs11@gmail.com>
2017-05-12 20:30:38 -0500

    such troubles

--- get_beer.R
+++ get_beer.R
@@ -2,7 +2,61 @@
 # Documentation
 # http://www.brewerydb.com/developers/docs
 
+library(tidyjson)
+library(jsonlite)
+library(httr)
+
 key <- "2302d0ab728f1b1aa664b9db6585885b"
 
+brewery_db <- handle(paste0("http://api.brewerydb.com/v2", key))
+add_key <- paste0("/?key=", key)
+GET(handle = brewery_db, path = "/beer/oeGSxs", add_headers())
 
-request <- paste0("http://api.brewerydb.com/v2/?key=", key)
+GET(handle = brewery_db, path = "/beer/oeGSxs")
+
+GET("http://api.brewerydb.com/v2/?key=2302d0ab728f1b1aa664b9db6585885b&beers")
+
+
+request <- paste0("http://api.brewerydb.com/v2/?key=", key, "&format=json", "/beer/oeGSxs")
+
+
+one_beer <- GET(request)
+one_beer
+
+
+fromJSON(paste0("http://api.brewerydb.com/v2/beers/?key=2302d0ab728f1b1aa664b9db6585885b"))
+
+fromJSON(request)
+
+
+
+a_beer <- GET("http://api.brewerydb.com/v2/beer/oeGSxs/?key=2302d0ab728f1b1aa664b9db6585885b")
+
+
+
+GET("http://api.brewerydb.com/v2/?key=2302d0ab728f1b1aa664b9db6585885b", path = "beers", verbose())
+
+GET("http://api.brewerydb.com/v2/?key=2302d0ab728f1b1aa664b9db6585885b", query = "Goosinator", verbose())
+
+
+
+
+GET("http://google.com/", path = "search", query = list(q = "Goosinator"))
+
+
+big_request <- paste0("http://api.brewerydb.com/v2/?key=", key, "&beers")
+
+
+all_beers <- GET(big_request)
+all_beers
+
+
+
+
+
+
+
+fromJSON("http://api.brewerydb.com/v2/beer/oeGSxs/?key=2302d0ab728f1b1aa664b9db6585885b")
+
+
+
