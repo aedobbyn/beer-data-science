fe245eecfe25800417bdc0071985dd44f496a992
aedobbyn <adobbs11@gmail.com>
2017-05-12 22:09:46 -0500

    make function that makes functions for getting certain queries by id)

--- get_beer.R
+++ get_beer.R
@@ -6,14 +6,7 @@ library(tidyjson)
 library(jsonlite)
 library(httr)
 
-key <- "2302d0ab728f1b1aa664b9db6585885b"
-
-brewery_db <- handle(paste0("http://api.brewerydb.com/v2"))
-add_key <- paste0("/?key=", key)
-GET(handle = brewery_db, path = "/beer/oeGSxs", add_headers())
-
-GET(handle = brewery_db, path = "/beer/oeGSxs")
-
+source("./key.R")
 
 base_url <- "http://api.brewerydb.com/v2"
 key_preface <- "/?key="
@@ -21,6 +14,97 @@ key_preface <- "/?key="
 
 request <- "/beer/oeGSxs"
 naughty_nienty <- fromJSON(paste0(base_url, request, key_preface, key))
+
+
+endpoints <- c("beers", "breweries", "categories", "events",
+                    "featured", "features", "fluidsizes", "glassware",
+                    "locations", "guilds", "heartbeat", "ingredients",
+                    "search", "search/upc", "socialsites", "styles")
+
+single_param_endpoints <- c("beer", "brewery", "category", "event",
+                          "feature", "glass", "guild", "ingredient",
+                          "location", "socialsite", "style", "menu")
+
+
+construct_request <- function(endpoint, id) {
+  request <- fromJSON(paste0(base_url, "/", endpoint, "/", id, key_preface, key))
+  return(request)
+}
+
+construct_request("brewery", "KR4X6i")
+construct_request("hop", "84")
+construct_request("beer", "oeGSxs")
+
+
+single_endpoint_request <- function() {
+  all_requests <- vector()
+  for (i in endpoints) {
+    this_request <- paste0(base_url, "/", i, "/", key_preface, key)
+    all_requests <- c(all_requests, this_request)
+  }
+  all_requests
+}
+
+single_endpoint_request()
+
+
+single_endpoint_request_funcs <- function(ep) {
+  # for (i in endpoints) {
+    this_request <- function() { fromJSON(paste0(base_url, "/", ep, "/", key_preface, key)) }
+    this_request
+  # }
+  # this_request
+}
+
+get_breweries <- single_endpoint_request_funcs("breweries")
+get_breweries()
+
+
+power <- function(ep) {
+  function(i) {
+    x ^ exponent
+  }
+}
+
+square <- power(2)
+square(2)
+
+
+power <- function(exponent) {
+  function(x) {
+    x ^ exponent
+  }
+}
+
+square <- power(2)
+square(2)
+
+
+
+simple_request_funcs <- function(ep) {
+  # for (i in endpoints) {
+  this_request <- function(id) { 
+    fromJSON(paste0(base_url, "/", ep, "/", id, "/", key_preface, key))
+    }
+  this_request
+  # }
+  # this_request
+}
+
+get_breweries <- simple_request_funcs("beer")
+get_breweries("oeGSxs")
+
+get_hops <- simple_request_funcs("hop")
+get_hops("84")
+
+
+
+myf <- function(x) {
+  innerf <- function(x) assign("Global.res", x^2, envir = .GlobalEnv)
+  innerf(x+1)
+}
+myf(3)
+
 
 
 
@@ -38,4 +122,5 @@ all_beers <- fromJSON("http://api.brewerydb.com/v2/beers/?key=2302d0ab728f1b1aa6
 
 hop_84 <- fromJSON("http://api.brewerydb.com/v2/hop/84/?key=2302d0ab728f1b1aa664b9db6585885b")
 
+hop_84 <- fromJSON("http://api.brewerydb.com/v2/hop/84/?key=2302d0ab728f1b1aa664b9db6585885b")
 
