7f8c66e8b17eff9a46a6bef69007fcbf5aaa2510
aedobbyn <adobbs11@gmail.com>
2017-05-20 17:40:32 -0500

    add unnest function

--- munge.R
+++ munge.R
@@ -8,17 +8,36 @@ all_beer_unnested <- str(all_beer$data)
 
 all_beer$data$glass <- all_beer$data$glass$id
 
+unnested_beer <- all_beer
 
 unnest_beer <- function() {
-  unnested_beer <- list()
   for(col in seq_along(all_beer$data)) {
-    # print(col)
-    # print(names(all_beer$data[col]))
     if(! is.null(ncol(all_beer[["data"]][[col]]))) {
+      print(all_beer[["data"]][[col]][["name"]])
+      unnested_beer[["data"]][[col]] <- all_beer[["data"]][[col]][["name"]]
+    }
+  }
+  unnested_beer
+}
+
+unnested <- unnest_beer()
+head(unnested[["data"]])
+
+
+
+
+
+
+unnest_beer <- function() {
+  # unnested_beer <- all_beer
+  # for(col in all_beer$data) {
+  #   # print(col)
+    # print(names(all_beer$data[col]))
+    if(! is.null(ncol(all_beer[["data"]][[]]))) {
       # print(ncol(all_beer$data[[col]]))
       print(all_beer[["data"]][[col]]$name)
       # print(all_beer[["data"]][[col]][1])
-      unnested_beer <- bind_cols(unnested_beer, foo = all_beer[["data"]][[col]]$name)
+      unnested_beer[["data"]][[col]] <- all_beer[["data"]][[col]]$name
     }
     # print(ncol(all_beer$data[col]))
   }
