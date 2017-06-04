1033ae24a32ecd648b2c7976b365dba850137347
aedobbyn <adobbs11@gmail.com>
2017-06-04 16:47:17 -0500

    something wrong w this approach to looping through all ingredients and doing it in one fell swoop

--- split_ingredients.R
+++ split_ingredients.R
@@ -4,33 +4,36 @@ library(stringr)
 
 simple_beer_necessities
 
-split_ingredients <- function(df, ingredient) {
+ingredients <- c("hop_name", "malt_name")
+
+split_ingredients <- function(df) {
   ncol_df <- ncol(df)
   
-  ingredient_split <- str_split(sbn[[ingredient]], ", ")    # this returns a list
-  num_new_cols <- max(lengths(ingredient_split))
+  for (ingredient in ingredients) {
 
-  new_col_names <- vector()
+    ingredient_split <- str_split(df[[ingredient]], ", ")    # this returns a list
+    num_new_cols <- max(lengths(ingredient_split))      # find out max number of instances of an ingredient per beer. this will be the number of columns we're adding.
   
-  for (num in 1:num_new_cols) {
-    this_col <- ncol_df + 1
-    
-    df[, this_col] <- NA
-    names(df)[this_col] <- paste0(ingredient, "_", num)
-    ncol_df <- ncol(df)
-    
-    for (row in seq_along(ingredient_split)) {
-      if (!is.null(hops_split[[row]][num])) {
-        df[row, this_col] <- ingredient_split[[row]][num]
+    for (num in 1:num_new_cols) {
+      this_col <- ncol_df + 1         # create a new column, initialize it with NAs, and name it
+      
+      df[, this_col] <- NA
+      names(df)[this_col] <- paste0(ingredient, "_", num)
+      ncol_df <- ncol(df)             # update the number of columns
+      
+      for (row in seq_along(ingredient_split)) {           # for each element in our list of split up ingredients
+        if (!is.null(ingredient_split[[row]][num])) {         # if it exists, add it to the correct column in our df
+          df[row, this_col] <- ingredient_split[[row]][num]
+        }
       }
     }
+    ncol_df <- ncol(df)
   }
-
   return(df)
 }
 
-
-sbn_added <- ingredient_split(sbn, "malt_name")
+sbn_added <- sbn
+sbn_added <- ingredient_split(sbn)
 View(sbn_added)
 
 
