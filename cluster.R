b4827e4b9c91bc4fdd55af1765a50116dd39bf85
aedobbyn <adobbs11@gmail.com>
2017-07-06 21:42:57 -0500

    split clustering function into a preper and a clusterer so we can show where 10 clusters came from

--- cluster.R
+++ cluster.R
@@ -274,3 +274,49 @@ ggplot() +
   labs(colour = "Style") +
   theme_bw()
 
+
+
+# **
+
+cluster_prep <- function(df, preds, to_scale, resp) {
+  # browser()
+  df_for_clustering <- df %>%
+    select_(.dots = c(response_vars, cluster_on)) %>%
+    na.omit() %>%
+    filter(
+      abv < 20 & abv > 3    # Only keep beers with ABV between 3 and 20 and an IBU less than 200
+    ) %>%
+    filter(
+      ibu < 200    
+    )
+  
+  df_all_preds <- df_for_clustering %>%
+    select_(.dots = preds)
+  
+  df_preds_scale <- df_all_preds %>%
+    select_(.dots = to_scale) %>%
+    rename(
+      abv_scaled = abv,
+      ibu_scaled = ibu,
+      srm_scaled = srm
+    ) %>%
+    scale() %>%
+    as_tibble()
+  
+  df_preds <- bind_cols(df_preds_scale, df_all_preds[, (!names(df_all_preds) %in% to_scale)])
+  
+  df_outcome <- df_for_clustering %>%
+    select_(.dots = resp) %>%
+    na.omit()
+  
+  cluster_prep_out <- list(preds = df_preds, outcome = df_outcome)
+  return(cluster_prep_out)
+}
+
+cluster_on <- c("abv", "ibu", "srm", "total_hops", "total_malt")
+to_scale <- c("abv", "ibu", "srm", "total_hops", "total_malt")
+response_vars <- c("name", "style", "style_collapsed")
+cp <- cluster_prep(df = beer_totals,
+                   preds = cluster_on,
+                   to_scale = to_scale,
+                   resp = response_vars)
