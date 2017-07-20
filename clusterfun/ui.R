a4e3223c615e343bb1895d570b4291863ed98886
aedobbyn <adobbs11@gmail.com>
2017-07-19 23:59:29 -0500

    trying to display function for collapsing styles inline

--- clusterfun/ui.R
+++ clusterfun/ui.R
@@ -11,21 +11,30 @@ style_names <- levels(popular_beer_dat$style_collapsed)
 names(style_names) <- levels(popular_beer_dat$style_collapsed)
 
 
+keywords <- c("Lager", "Pale Ale", "India Pale Ale", "Double India Pale Ale", "India Pale Lager", "Hefeweizen", "Barrel-Aged","Wheat", "Pilsner", "Pilsener", "Amber", "Golden", "Blonde", "Brown", "Black", "Stout", "Porter", "Red", "Sour", "Kölsch", "Tripel", "Bitter", "Saison", "Strong Ale", "Barley Wine", "Dubbel", "Altbier")
+
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
   
   theme = shinytheme("spacelab"),
   
   titlePanel("Clustered Beer"),
-  p("Choose the beer style you'd like to see and the number of clusters you "),
-  
+  p("Beers from the BreweryDB API. To drill down into a certain style, uncheck the 'Show all styles'
+  checkbox and choose a beer style from the dropdown. Rerun the algorithm using any number of cluster
+  centers by changing the Number of Clusters."),
+  br(),
+  # p("Beers were collapsed into these styles using this function:"),
+  # br(),
+  # pre(renderText("../keywords.txt")),
+  # br(),
+  # br(),
+
+ 
   sidebarLayout(
     sidebarPanel(
       checkboxInput("show_all", "Show all styles", TRUE),
       
       numericInput("num_clusters", "Number of Clusters:", 4),
-      
-      
       
       conditionalPanel(
         condition = "input.show_all == false",
