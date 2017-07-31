551b50db7e44a912a89515d21328d7f416f6c499
aedobbyn <adobbs11@gmail.com>
2017-07-30 22:38:37 -0500

    delete extra commented out code in cluster_prep

--- clusterfun/cluster_prep.R
+++ clusterfun/cluster_prep.R
@@ -29,8 +29,6 @@ popular_beer_dat <- factorize_cols(popular_beer_dat)
 beer_totals <- factorize_cols(beer_totals)
 
 
-
-<<<<<<< HEAD
 # set types
 beer_totals$style <- factor(beer_totals$style)
 beer_totals$glass <- factor(beer_totals$glass)
@@ -47,60 +45,9 @@ beer_totals$hops_name <- factor(beer_totals$hops_name)
 beer_totals$malt_name <- factor(beer_totals$malt_name)
 
 
-# factorize_ingredients <- function(df) {
-#   for(col_name in names(df)) {
-#     if (grepl(("hops_name_|malt_name_|style|glass"), col_name) == TRUE) {
-#       df[[col_name]] <- factor(df[[col_name]])
-#     }
-#   }
-#   return(df)
-# }
-# 
-# beer_totals <- factorize_ingredients(beer_totals)
 
-
-
-
-
-# prep_clusters <- function(df, preds, to_scale, resp) {
-#   df_for_clustering <- df %>%
-#     select_(.dots = c(response_vars, cluster_on)) %>%
-#     na.omit() %>%
-#     filter(
-#       abv < 20 & abv > 3    # Only keep beers with ABV between 3 and 20 and an IBU less than 200
-#     ) %>%
-#     filter(
-#       ibu < 200    
-#     )
-#   
-#   df_all_preds <- df_for_clustering %>%
-#     select_(.dots = preds)
-#   
-#   df_preds_scale <- df_all_preds %>%
-#     select_(.dots = to_scale) %>%
-#     rename(
-#       abv_scaled = abv,
-#       ibu_scaled = ibu
-#       # srm_scaled = srm
-#     ) %>%
-#     scale() %>%
-#     as_tibble()
-#   
-#   df_preds <- bind_cols(df_preds_scale, df_all_preds[, (!names(df_all_preds) %in% to_scale)])
-#   
-#   df_outcome <- df_for_clustering %>%
-#     select_(.dots = resp) %>%
-#     na.omit()
-#   
-#   cluster_prep_out <- list(df_for_clustering = df_for_clustering, preds = df_preds, outcome = df_outcome)
-#   
-#   return(cluster_prep_out)
-# }
-=======
 # response_vars <- c("name", "style", "style_collapsed")
 
-
->>>>>>> dev
 
 
 
